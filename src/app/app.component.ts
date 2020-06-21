a242dfece125171a39586f191fa18cf3d64d728a
Kashyap Sistla <kashyap.sistla@live.com>
2020-06-21 14:15:33 -0500

    commit services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
-
+import { KsPlatformService } from 'ks-platform-service';
+import { KsRootService } from 'ks-root-service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'provide-in-service-spa';
+  constructor(public platformService: KsPlatformService,
+              public rootService: KsRootService) { }
+  pValue = 0;
+  rValue = 0;
+  updatePlatformValue() {
+    let val = this.platformService.getValue();
+    val++;
+    this.platformService.setValue(val);
+  }
+
+  getPlatformValue() {
+    this.rValue = this.rootService.getValue();
+  }
+
+
+  updateRootValue() {
+    let val = this.rootService.getValue();
+    val++;
+    this.rootService.setValue(val);
+  }
+
+  getRootValue() {
+    this.pValue = this.platformService.getValue();
+  }
 }
